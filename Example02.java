b448c3b114606b46188a3c5a4cf1f617d73a7f29
Renew12 <49063181+Renew12@users.noreply.github.com>
2019-03-31 14:52:00 +0800

    上传一个java
    真好

---
+++ Example02.java
@@ -0,0 +1,13 @@
+public class Example02 {
+	public static void main (String[] arges) {
+			byte a = 10;
+			int b;
+			float f = 12.5f;
+			double d;
+			b = a;
+			d = a + b + f;
+			System.out.println(b);
+			System.out.println(d);
+			
+	}
+}
